3532ce498ee16c5e7aec2d232e565f8bfd0c26f7
RobertKashyap <orionalchemist@gmail.com>
2021-04-18 14:44:30 +0530

    Did some basic initial configurations to aid workflow as per the workspace and editor requirement..added CMake and build.sh files along with folder structures

---
+++ CMakeFiles/implemented.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/implemented.dir/src/todo.cpp.o"
+  "implemented.pdb"
+  "implemented"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/implemented.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/implemented.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for implemented.
+# This may be replaced when dependencies are built.
